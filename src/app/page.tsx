bfbf2ba340392cb398cea443f9560da86bf4f148
Melanie van de Graaf <i499892@fontysict.nl>
2025-10-20 18:15:40 +0200

    chore: made component of project card
    made component of project card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,27 @@
 import {cn} from "@/lib/utils";
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card"
+import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle,} from "@/components/ui/card"
+import {ProjectCard} from "@/components/project-card";
 
 export default function Home() {
     const sectionClassName = "min-h-[calc(100lvh-40px)]"
 
     return (
-        <div className={"flex flex-col mt-10 overflow-y-scroll h-[calc(100lvh-40px)] snap-y"}>
+        <div className={"flex flex-col mt-10 overflow-y-scroll h-[calc(100lvh-40px)] snap-y snap-proximity"}>
             <section id={"melanie"} className={cn(sectionClassName, "snap-start bg-gray-200")}>
-                <p className={"bg-purple-100 w-fit p-1 mt-10 mb-10"} style={{borderRadius: '15px'}} >Available for opportunities</p>
+                <p className={"bg-purple-100 w-fit p-1 mt-10 mb-10"} style={{borderRadius: '15px'}}>Available for
+                    opportunities</p>
                 <h2 className={"font-bold text-5xl mb-2"}>Hi, I'm Melanie</h2>
                 <p className={"font-bold text-gray-500 mb-5 text-2xl"}>Fun facts/ Inspirational quote</p>
-                <p className={" outline outline-2 outline-purple-500"} style={{borderRadius: '15px'}}>Cras vel augue rhoncus, imperdiet libero eu,
-                    viverra ligula. Vestibulum  ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia  curae; Vivamus malesuada ex dui.
-                    Phasellus augue urna, aliquet in  placerat eu, molestie at neque. Quisque tincidunt porta mauris ut  dictum. Mauris lorem
+                <p className={" outline outline-2 outline-purple-500"} style={{borderRadius: '15px'}}>Cras vel augue
+                    rhoncus, imperdiet libero eu,
+                    viverra ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+                    curae; Vivamus malesuada ex dui.
+                    Phasellus augue urna, aliquet in placerat eu, molestie at neque. Quisque tincidunt porta mauris ut
+                    dictum. Mauris lorem
                     mi, commodo vitae sem at</p>
-                    <button className={"bg-purple-600 mt-5 text-white p-1"} style={{borderRadius: '15px'}} >View my projects</button>
+                <button className={"bg-purple-600 mt-5 text-white p-1"} style={{borderRadius: '15px'}}>View my
+                    projects
+                </button>
             </section>
 
             <section id={"about"} className={cn(sectionClassName, "snap-start bg-blue-100")}>
@@ -31,118 +31,27 @@ export default function Home() {
                 <div className="flex justify-between">
                     <div className="w-1/2 flex flex-col">
                         <p className={"font-bold text-2xl text-center mb-1 text-left"}>My Story</p>
-                        <p className={"mr-50"}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam posuere  eleifend ex eu tincidunt.
-                            In eleifend luctus blandit. Maecenas sollicitudin  ac augue at viverra. Nullam sodales eros et ornare lobortis.
-                            Aenean  euismod congue lacus, a tincidunt nisl porta nec. Aliquam interdum id  enim ut consequat.viverra ligula.
-                            Vestibulum  ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia  curae; Vivamus malesuada ex dui.
-                            Phasellus augue urna, aliquet in  placerat eu,
-                            molestie at neque. Quisque tincidunt porta mauris ut  dictum. Mauris lorem </p>
+                        <p className={"mr-50"}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam posuere
+                            eleifend ex eu tincidunt.
+                            In eleifend luctus blandit. Maecenas sollicitudin ac augue at viverra. Nullam sodales eros
+                            et ornare lobortis.
+                            Aenean euismod congue lacus, a tincidunt nisl porta nec. Aliquam interdum id enim ut
+                            consequat.viverra ligula.
+                            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
+                            Vivamus malesuada ex dui.
+                            Phasellus augue urna, aliquet in placerat eu,
+                            molestie at neque. Quisque tincidunt porta mauris ut dictum. Mauris lorem </p>
                     </div>
 
                     <div className="w-1/2 flex flex-col">
                         <p className={"font-bold text-2xl text-center mb-1"}>Hobbies and interests</p>
                         <div className="grid grid-cols-2 grid-rows-3 gap-4">
-                            <Card>
-                                <CardHeader>
-                                    <CardTitle>Project name</CardTitle>
-                                    <CardDescription>
-                                        This is a project description
-                                    </CardDescription>
-                                </CardHeader>
-                                <CardContent>
-                                    This is the card content
-                                </CardContent>
-                                <CardFooter>
-                                    <button variant="outline" className="w-full">
-                                        Go to project
-                                    </button>
-                                </CardFooter>
-                            </Card>
-
-                            <Card>
-                                <CardHeader>
-                                    <CardTitle>Project name</CardTitle>
-                                    <CardDescription>
-                                        This is a project description
-                                    </CardDescription>
-                                </CardHeader>
-                                <CardContent>
-                                    This is the card content
-                                </CardContent>
-                                <CardFooter className="flex-col gap-2">
-                                    <button variant="outline" className="w-full">
-                                        Go to project
-                                    </button>
-                                </CardFooter>
-                            </Card>
-
-                            <Card>
-                                <CardHeader>
-                                    <CardTitle>Project name</CardTitle>
-                                    <CardDescription>
-                                        This is a project description
-                                    </CardDescription>
-                                </CardHeader>
-                                <CardContent>
-                                    This is the card content
-                                </CardContent>
-                                <CardFooter className="flex-col gap-2">
-                                    <button variant="outline" className="w-full">
-                                        Go to project
-                                    </button>
-                                </CardFooter>
-                            </Card>
-
-                            <Card>
-                                <CardHeader>
-                                    <CardTitle>Project name</CardTitle>
-                                    <CardDescription>
-                                        This is a project description
-                                    </CardDescription>
-                                </CardHeader>
-                                <CardContent>
-                                    This is the card content
-                                </CardContent>
-                                <CardFooter className="flex-col gap-2">
-                                    <button variant="outline" className="w-full">
-                                        Go to project
-                                    </button>
-                                </CardFooter>
-                            </Card>
-
-                            <Card>
-                                <CardHeader>
-                                    <CardTitle>Project name</CardTitle>
-                                    <CardDescription>
-                                        This is a project description
-                                    </CardDescription>
-                                </CardHeader>
-                                <CardContent>
-                                    This is the card content
-                                </CardContent>
-                                <CardFooter className="flex-col gap-2">
-                                    <button variant="outline" className="w-full">
-                                        Go to project
-                                    </button>
-                                </CardFooter>
-                            </Card>
-
-                            <Card>
-                                <CardHeader>
-                                    <CardTitle>Project name</CardTitle>
-                                    <CardDescription>
-                                        This is a project description
-                                    </CardDescription>
-                                </CardHeader>
-                                <CardContent>
-                                    This is the card content
-                                </CardContent>
-                                <CardFooter className="flex-col gap-2">
-                                    <button variant="outline" className="w-full">
-                                        Go to project
-                                    </button>
-                                </CardFooter>
-                            </Card>
+                            <ProjectCard projectId={""} projectName={"Project"} projectDescription={"This is a project"}/>
+                            <ProjectCard projectId={""} projectName={"Project"} projectDescription={"This is a project"}/>
+                            <ProjectCard projectId={""} projectName={"Project"} projectDescription={"This is a project"}/>
+                            <ProjectCard projectId={""} projectName={"Project"} projectDescription={"This is a project"}/>
+                            <ProjectCard projectId={""} projectName={"Project"} projectDescription={"This is a project"}/>
+                            <ProjectCard projectId={""} projectName={"Project"} projectDescription={"This is a project"}/>
                         </div>
                     </div>
                 </div>
@@ -246,7 +155,8 @@ export default function Home() {
 
             <section id={"proficiencies"} className={cn(sectionClassName, "snap-start bg-blue-100")}>
                 <h2 className={"font-bold text-5xl text-center mb-1"}> Proficiencies</h2>
-                <p className={"font-bold text-gray-500 text-center mb-10"}>My confidence and experience with technologies</p>
+                <p className={"font-bold text-gray-500 text-center mb-10"}>My confidence and experience with
+                    technologies</p>
                 <div className="grid grid-cols-2 grid-rows-5 gap-10">
                     <p>C#</p>
                     <p>Skill</p>
---
+++ src/components/project-card.tsx
@@ -0,0 +1,22 @@
+import {Card, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
+import {Button} from "@/components/ui/button";
+
+export function ProjectCard(props: {
+    projectName: string;
+    projectDescription: string;
+    projectId: string;
+}) {
+    return <Card className={"p-3"}>
+        <CardHeader className={"px-1"}>
+            <CardTitle>{props.projectName}</CardTitle>
+            <CardDescription>
+                {props.projectDescription}
+            </CardDescription>
+        </CardHeader>
+        <CardFooter>
+            <Button variant="project" className="w-full">
+                Go to project
+            </Button>
+        </CardFooter>
+    </Card>;
+}
