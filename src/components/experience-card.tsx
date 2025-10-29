e6cd521cc8de789c72c3a82c17cf8e68aa07f218
Melanie <melanievandegraaf1@live.nl>
2025-10-29 13:24:35 +0100

    feat: added dialog to experience card, added different kinds description

--- src/components/experience-card.tsx
+++ src/components/experience-card.tsx
@@ -1,24 +1,30 @@
 import {Card, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
 import {Badge} from "@/components/ui/badge";
 import React from "react";
+import {ProjectDialog} from "@/components/project-dialog";
+
 export enum ExperienceCardVariant {
     Work = 0,
     Education = 1,
 }
 
 export interface ExperienceCardProps {
+    title: string;
     name: string;
-    description: string;
-    tags: string[];
+    smallDescription: string;
+    longDescription: string;
     variant: ExperienceCardVariant;
 }
 
 export function ExperienceCard({
-                                name,
-                                description,
-                                tags,
-                                variant,
-                            }: Readonly<ExperienceCardProps>) {
+                                   title,
+                                   name,
+                                   smallDescription,
+                                   longDescription,
+                                   variant,
+                               }:
+                               Readonly<ExperienceCardProps>
+) {
     let titleString: string;
 
     switch (variant) {
@@ -41,17 +47,25 @@ export function ExperienceCard({
             </div>
             <CardHeader className={"p-3 px-4 pb-0"}>
                 <CardTitle className={"text-foreground pt-0"}>
-                    {name}
+                    <p className={"mb-2"}>
+                        {name}
+                    </p>
+                    <p>
+                        {title}
+                    </p>
                 </CardTitle>
                 <CardDescription className={"text-foreground pt-0"}>
-                    {description}
+                    {smallDescription}
                 </CardDescription>
             </CardHeader>
             <CardFooter className={"grid grid-cols-4 gap-1 p-3 pb-2 pt-0"}>
-                {tags.map((tag) => (
-                    <Badge className={"bg-accent text-accent-foreground w-full"} key={tag}>{tag}</Badge>))}
             </CardFooter>
-            <div className={"grid grid-cols-2 p-2 gap-5"}>
+            <div className={"flex justify-center mb-2"}>
+                <ProjectDialog
+                    title={title}
+                    projectName={name}
+                    longDescription={longDescription}
+                />
             </div>
         </Card>
     )
